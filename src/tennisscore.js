3c3f0faa52472f0a1df3049dff90b69538749564
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-28 09:47:01 -0400

    feat: devuelve 15-Love cuando el jugador 1 marca un punto

--- src/tennisscore.js
+++ src/tennisscore.js
@@ -1,4 +1,7 @@
-function showScore(){
+function showScore(jugador1, jugador2) {
+    if(jugador1 ===1 && jugador2 ===0) {
+        return "15 - Love";
+    }
     return "Love - Love";
 }
 export default showScore;
--- src/tennisscore.spec.js
+++ src/tennisscore.spec.js
@@ -2,6 +2,11 @@ import showScore from "./tennisscore.js";
 describe("TennisScore", () => {
     //Empieza sin puntos
     it("Deberia empezar sin puntos Love-Love", () => {
-        expect(showScore()).toEqual("Love - Love");
+        expect(showScore(0,0)).toEqual("Love - Love");
     });
+    //Jugador 1 gana un punto
+    it("Deberia mostrar 15-Love cuando el jugador 1 gana un punto", () => { 
+        expect(showScore(1,0)).toEqual("15 - Love");
+    });
+    
 });
