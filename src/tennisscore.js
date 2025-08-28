ee47aea902ac96d63ddfceac7bbd6c82fc674953
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-28 10:19:28 -0400

    refactoring: funciona la primera prueba cuando el jugador 1 y el jugador 2 empiezan en 0 devuelve Love- Love

--- src/tennisscore.js
+++ src/tennisscore.js
@@ -1,7 +1,18 @@
-function showScore(jugador1, jugador2) {
-    if(jugador1 ===1 && jugador2 ===0) {
-        return "15 - Love";
-    }
+
+//OBJETIVO 
+class TennisScorer{
+   // this.pointsPlayer1 = 0;
+    //this.pointsPlayer2 = 0;
+    //player1Scores();{
+
+    //}
+    //player2Scores();{}
+    showScore() {
+   // if(jugador1 ===1 && jugador2 ===0) {
+      //  return "15 - Love";
+    //}
+
     return "Love - Love";
-}
-export default showScore;
+   }
+};
+export default TennisScorer;
--- src/tennisscore.spec.js
+++ src/tennisscore.spec.js
@@ -1,12 +1,18 @@
-import showScore from "./tennisscore.js";
-describe("TennisScore", () => {
+
+import TennisScorer from "./tennisscore.js";
+
+describe("TennisScorer", () => {
     //Empieza sin puntos
     it("Deberia empezar sin puntos Love-Love", () => {
-        expect(showScore(0,0)).toEqual("Love - Love");
+        const scorer= new TennisScorer();
+        expect(scorer.showScore()).toEqual("Love - Love");
     });
+    /*
     //Jugador 1 gana un punto
     it("Deberia mostrar 15-Love cuando el jugador 1 gana un punto", () => { 
         expect(showScore(1,0)).toEqual("15 - Love");
     });
-    
+    it("Deberia mostrar 30-Love cuando el jugador 2 gana dos puntos", () => {
+        expect(showScore(2,0)).toEqual("30 - Love");
+    });*/
 });
